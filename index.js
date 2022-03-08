0b934097fe337b575105f047b642282b39f3047b
Vinycius Osadczuk Padilha <vinyosadczuk@hotmail.com>
2022-03-08 15:09:45 -0300

    Reorganização para que index apenas suba o servidor

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-//projeto já conta com nodemon, de modo a não precisar reiniciar o servidor a cada alteração.
-//express instalado para fazer a rota.
-//chamando a biblioteca
-const express = require('express')
-const consign = require('consign')
-//executa o express recém importado
-const app = express()
-
-//consign para organizar os arquivos
-consign()
-  .include('controllers')
-  .into(app)
-
 //subindo o servidor
+
+const customExpress = require('./config/customExpress')
+
+const app = customExpress()
+
+
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
