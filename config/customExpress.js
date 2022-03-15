531571ebda6462dafd1cc7a008002503d9d8b0df
Vinycius Osadczuk Padilha <vinyosadczuk@hotmail.com>
2022-03-15 15:14:31 -0300

    Configurando o servidor.

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,19 +1,21 @@
 // arquivo de configurações do app/servidor
 
-
-
 //projeto já conta com nodemon, de modo a não precisar reiniciar o servidor a cada alteração.
 //express instalado para fazer a rota.
 //chamando a biblioteca
 
-
+//bibliotecas externas
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 
 //executa o express recém importado
 module.exports = () => {
   const app = express()
+
+  app.use(bodyParser.urlencoded({extended:true}))
+  app.use(bodyParser.json())
 
   //consign para organizar os arquivos
   consign()
